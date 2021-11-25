bded2a80f4a96a66c64f15bb26216bb679eae8e3
mamo1986 <94875025+mamo1986@users.noreply.github.com>
2021-11-24 20:43:23 -0500

    Update one.java
    i add it new line to code

--- src/demo1/one.java
+++ src/demo1/one.java
@@ -3,6 +3,7 @@ package demo1;
 public class one {
 	public static void main(String[] args) {
 		System.out.println("project one is change ....");
+		System.out.println("i just need to say hi .");
 	}
 
 }
